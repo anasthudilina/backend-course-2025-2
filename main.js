6072d822db5f5d553fe9f8fe5c013bceb0d2cff5
Anastasiia Hudilina <anastasiia.hudilina@lnu.edu.ua>
2026-03-04 21:41:27 +0200

    Створено main.js для генерації URL

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 
 const date = "202202";
 
-
-
 const nbuUrl = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/res");
-
 
 
 nbuUrl.searchParams.append("date", date);
 
 nbuUrl.searchParams.append("json", "");
-
 
 
 console.log(nbuUrl.toString());
