3c7eb9e0ce82bfde9babf93c1384b39f935c16c5
Anastasia bc2025-2 <anastasia.gudilina.i@gmail.com>
2026-03-04 21:39:21 +0200

    Створено main.js для генерації URL

---
+++ main.js
@@ -0,0 +1,17 @@
+
+const date = "202202";
+
+
+
+const nbuUrl = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/res");
+
+
+
+nbuUrl.searchParams.append("date", date);
+
+nbuUrl.searchParams.append("json", "");
+
+
+
+console.log(nbuUrl.toString());
+
